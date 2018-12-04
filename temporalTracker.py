25f63ae759f19aca23e4d6e71747d20643f87a8b
bronsk33t <bpcalaway@gmail.com>
2018-12-03 20:22:16 -0600

    Bad solution but honestly who cares

--- temporalTracker.py
+++ temporalTracker.py
@@ -11,56 +11,37 @@ class temporalTracker:
     def incrementer(self, frequency):
         self.__changelog.append(self.attunement)
         self.attunement = self.attunement + frequency
-        if self.attunement in self.__changelog:
-            self.__firstChild = self.attunement
 
     #I don't remember any of this algorithms shit but I have plenty of memory lmao.
-    def getFirstMatch(self):
-        popList = []
-        pushList = self.__changelog
-        #Whoops
-        pushList.append(self.attunement)
-        for attunement in pushList:
-            if attunement in popList:
-                return attunement
-            else:
-                popList.append(attunement)
-                pushList = pushList[1:]
+    def getFirstMatch(self, inputs):
+        for frequency in inputs:
+            self.incrementer(int(frequency))
+            if self.attunement in self.__changelog:
+                return self.attunement
+        return 0
         #We've failed to find a match, we must continue to grow the list of frequencies.
         
     
     def getFirstChild(self):
-        return self.__firstChild
+        return self.__changelog
             
 
 tt = temporalTracker()
 fh = open("inputs/day1", 'r')
-testarr = []
-for line in fh.readlines():
+#inputs = fh.readlines()
+inputs = [3, 3, 4, -2, -4]
+for line in inputs:
     tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-for line in fh.readlines():
-    tt.incrementer(int(line))
-
 print(tt.attunement)
-print(tt.getFirstMatch())
-print(tt.getFirstChild())
+
+first = 0
+iter = 0
+while not first:
+    first = tt.getFirstMatch(inputs)
+    iter = iter + 1
+    if iter > 200:
+        break
+#print(tt.getFirstChild())
+
+print(len(tt.getFirstChild()))
+print(first)
